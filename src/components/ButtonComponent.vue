271ca5704f2d1242c38c46bf2e33c1b31ca16882
anastasiia <krestina.av@yandex.ru>
2022-12-27 17:14:04 +0400

    added project

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,34 @@
+<script>
+import { RouterLink } from 'vue-router';
+
+export default {
+    props: {
+        title: {
+            type: String,
+            default: "Отправить письмо",
+        } 
+    },
+
+    data() {
+        return {}
+    },
+
+    methods: {}
+}
+</script>
+
+<template>    
+    <div class="button">
+        <span>
+            {{ title }}
+        </span>
+    </div>
+</template>
+
+<style>
+.button {
+    padding: 15px;
+    background-color: blueviolet;
+    border-radius: 20px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<script>
+import ButtonComponent from '../components/ButtonComponent.vue';
+
+export default {
+  components: {
+    ButtonComponent,
+  }, 
+
+  data() {
+        return {}
+    },
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="contain">
+      (HOVER)
+      <button-component>
+      </button-component>
+    </div>
+  </div>
+</template>
+
+<style>
+.container {
+  margin: 0 auto;
+  max-width: 1548px;
+}
+
+.contain {
+  margin-top: 50px;
+  padding: 72px 95px;
+  border: 8px solid var(--white);
+}
+</style>
+

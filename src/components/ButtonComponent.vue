c9c5755e1ffc327f71693775aca14a83cc04968c
anastasiia <krestina.av@yandex.ru>
2022-12-28 17:41:53 +0400

    [+] variants for button color

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -1,25 +1,89 @@
 <script>
-import { RouterLink } from 'vue-router';
+//import { RouterLink } from 'vue-router';
 
 export default {
     props: {
         title: {
             type: String,
             default: "Отправить письмо",
-        } 
+        },
+
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+
+        primary: {
+            type: Boolean,
+            default: false,
+        },
+
+        secondary: {
+            type: Boolean,
+            default: false,
+        },
+
+        warning: {
+            type: Boolean,
+            default: false,
+        },
+
+        info: {
+            type: Boolean,
+            default: false,
+        },
+
+        danger: {
+            type: Boolean,
+            default: false,
+        },
+
+        action: {
+            type: Boolean,
+            default: false,
+        },
+
+        question: {
+            type: Boolean,
+            default: false,
+        },
     },
 
     data() {
         return {}
     },
 
-    methods: {}
+    computed: {
+        className() {
+            return {
+                'button__disabled': this.disabled,
+                'button__color-primary': this.primary,
+                'button__color-secondary': this.secondary,
+                'button__color-warning': this.warning,
+                'button__color-info': this.info,
+                'button__color-danger': this.danger,
+                'button__color-action': this.action,
+                'button__color-question': this.question,
+
+            }
+        }
+    },
+
+    methods: {
+        click() {
+            this.$emit( 'click')
+        }
+    }
 }
 </script>
 
-<template>    
-    <div class="button">
-        <span>
+<template>
+    <div 
+    @click="click"
+    class="button"
+    :class="className"
+    >
+        <span class="button__text">
             {{ title }}
         </span>
     </div>
@@ -27,8 +91,42 @@ export default {
 
 <style>
 .button {
-    padding: 15px;
-    background-color: blueviolet;
+    padding: 15px 46px;
+    cursor: pointer;
     border-radius: 20px;
 }
+
+.button__disabled {
+    background: var(--disabled);
+    color: var(--disabled-text);
+}
+
+.button__color-primary {
+    background: var(--primary);
+}
+
+.button__color-secondary {
+    background: var(--secondary);
+}
+
+.button__color-warning {
+    background: var(--warning);
+}
+
+.button__color-info {
+    background: var(--info);
+}
+
+.button__color-danger {
+    background: var(--danger);
+}
+
+.button__color-action {
+    background: var(--action);
+}
+
+.button__color-question {
+    background: var(--question);
+}
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,17 +7,91 @@ export default {
   }, 
 
   data() {
-        return {}
-    },
+    return {}
+  },
+
+  methods: {
+    clickBtn() {
+      console.log('Button clicked')
+    }
+  }
 }
 </script>
 
 <template>
   <div class="container">
     <div class="contain">
-      (HOVER)
-      <button-component>
-      </button-component>
+      <div class="contain__button">
+        <h1 class="button_heading">(BUTTON CLASSIC)</h1>
+        <div class="button__variant">
+          <p class="button_title">(PRIMARY)</p> 
+          <button-component
+            @click="clickBtn"
+            :disabled="false"
+            :primary="true"
+          >
+          </button-component>
+        </div>
+        
+        <div class="button__variant">
+          <p class="button_title">(secondary)</p> 
+          <button-component
+            @click="clickBtn"
+            :disabled="false"
+            :secondary="true"
+          >
+          </button-component>
+        </div>
+
+        <div class="button__variant">
+          <p class="button_title">(warning)</p> 
+          <button-component
+            @click="clickBtn"
+            :disabled="false"
+            :warning="true"
+          >
+          </button-component>
+        </div>
+
+        <div class="button__variant">
+          <p class="button_title">(disabled)</p> 
+          <button-component
+            @click="clickBtn"
+            :disabled="true"
+          >
+          </button-component>
+        </div>
+
+        <div class="button__variant">
+            <p class="button_title">(info)</p> 
+            <button-component
+              @click="clickBtn"
+              :disabled="false"
+              :info="true"
+            >
+            </button-component>
+          </div>
+
+        <div class="button__variant">
+          <p class="button_title">(danger)</p> 
+          <button-component
+            @click="clickBtn"
+            :disabled="false"
+            :danger="true"
+          >
+          </button-component>
+        </div>
+
+        <div class="button__variant">
+          <p class="button_title">(action)</p> 
+          <button-component
+            @click="clickBtn"
+            :disabled="false"
+            :action="true"
+          >
+          </button-component>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -29,9 +103,35 @@ export default {
 }
 
 .contain {
-  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: baseline;
+  margin: 50px 0;
   padding: 72px 95px;
   border: 8px solid var(--white);
+}
+
+.contain__button {
+  text-align: center;
+}
+
+.button_heading {
+  padding-bottom: 18px;
+  text-align: center;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
+  color: var(--disabled);
+}
+
+.button__variant {
+  padding-bottom: 24px;
+}
+
+.button_title {
+  padding-bottom: 8px;
+  text-align: left;
 }
 </style>
 
